0d2e0bcf8217e9bf5db07fa243172a29fff2b29c
KosmiderPrzemyslaw <kosmiderprzemyslaw91@gmail.com>
2024-01-14 04:31:27 +0100

    create project car shop thymeleaf

---
+++ src/main/java/pl/kosmider/carshopthymeleaf/CarShopThymeleafApplication.java
@@ -0,0 +1,13 @@
+package pl.kosmider.carshopthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarShopThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarShopThymeleafApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kosmider/carshopthymeleaf/CarShopThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kosmider.carshopthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarShopThymeleafApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
